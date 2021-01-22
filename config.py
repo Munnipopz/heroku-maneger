32d2f7d77b59db2b77a851026347c38c3d2a204b
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-01-22 23:28:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","")
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY","")
-    AUTHORIZED_USERS = [int(user) for user in os.environ.get("AUTHORIZED_USERS").split(" ")]
+    BOT_TOKEN = os.environ.get("1572046148:AAFggc8jpkgKoeN2uqg2MFH8OAyBIIVlTS8","")
+    HEROKU_API_KEY = os.environ.get("ed4a7951-08b0-43f2-95c3-72b4a9c2c4c8","")
+    AUTHORIZED_USERS = [int(user) for user in os.environ.get("1575148105").split(" ")]
     TG_CHARACTER_LIMIT = 4000 
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","")
+    HEROKU_APP_NAME = os.environ.get("herokuuua","")
